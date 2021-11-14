6b81cbabbce1d6e28febd570f3eb027aafe39f60
Peter Yakovlev <gorkiy@gmail.com>
2021-11-14 13:17:13 +0300

    Initial setup for processSeq helper

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,37 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
 
+import { anyPass, allPass, compose, prop, equals, filter, values, lt, gt, not } from 'ramda';
+
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+// Values
+const getWriteLog = prop('writeLog');
+const getValue = prop('value');
+const getResult = prop('result');
+const getHandleSuccess = prop('handleSuccess');
+const getHandleError = prop('handleError');
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+const processSequence = input => {
+    //    {value, writeLog, handleSuccess, handleError}
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
-
-    wait(2500).then(() => {
-        writeLog('SecondLog')
-
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
-
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+    const log = getWriteLog(input);
+    const logInput = compose(log, getValue);
 }
 
 export default processSequence;
